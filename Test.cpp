488f2aabeaf12a43201331a4e52cc45d9f678a22
Solchild <Soulchild136@gmail.com>
2018-04-17 23:30:38 -0400

    Create Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,31 @@
+# Test
+#include <iostream>
+#include <string>
+#include <stdexcept>
+using namespace std;
+
+int main()
+{
+  string numBin = "";
+  int numDec;
+  int quotient;
+  
+  cout << "Enter your base 10 number." << endl;
+  cin >> numDec;
+  quotient = numDec;
+  try{
+  	if(numDec < 0)
+  		throw runtime_error("Invalid base 10 number.");
+  }
+  catch(runtime_error &except){
+  	cout << except.what() << endl;
+  	cout << "Cannot convert this number to binary." << endl;
+  }
+  while(quotient > 0){
+      if(quotient % 2 == 0)
+        numBin = "0" + numBin;
+      else numBin = "1" + numBin;
+      quotient = quotient / 2;
+  }
+  cout << numDec << " in binary = " << numBin;
+}
